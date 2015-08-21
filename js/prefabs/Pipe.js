9a76d6009c495a17b3ae018d5c9c37466974e5fc
Pappa <dr.pappa@gmail.com>
2015-08-21 15:47:39 +0100

    Improving pipe randomisation

--- js/prefabs/Pipe.js
+++ js/prefabs/Pipe.js
@@ -2,27 +2,27 @@ var FartyTurd = FartyTurd || {};
 
 FartyTurd.Pipe = function(game, x, y, speed, pipeData) {
   Phaser.Group.call(this, game);
-  
-  this.tileSize = 40;
+
   this.game = game;
   this.enableBody = true;
   this.isScored = false;
+  this.totalHeight = this.game.world.height - 30;
   this.pipeData = pipeData;
-  this.createPipe(x);
+  this.createPipe();
 
-  this.configure(x, y, speed, pipeData);  
+  this.configure(x, y, speed, pipeData);
 };
 
 FartyTurd.util.extend(FartyTurd.Pipe, Phaser.Group);
 
-FartyTurd.Pipe.prototype.createPipe = function(x) {
+FartyTurd.Pipe.prototype.createPipe = function() {
   var pipeParts = [];
-  pipeParts[0] = new Phaser.Sprite(this.game, x, 0, 'pipe');
-  pipeParts[0].height = 60;
-  pipeParts[1] = new Phaser.Sprite(this.game, x, 60, 'pipe-down');
-  pipeParts[2] = new Phaser.Sprite(this.game, x, 180, 'pipe-up');
-  pipeParts[3] = new Phaser.Sprite(this.game, x, 206, 'pipe');
-  pipeParts[3].height = this.game.height - 206;
+  pipeParts[0] = new Phaser.Sprite(this.game, 0, 0, 'pipe'); //0
+  pipeParts[1] = new Phaser.Sprite(this.game, 0, 0, 'pipe-down'); //60
+  pipeParts[1].height = 26;
+  pipeParts[2] = new Phaser.Sprite(this.game, 0, 0, 'pipe-up'); //180
+  pipeParts[2].height = 26;
+  pipeParts[3] = new Phaser.Sprite(this.game, 0, 0, 'pipe'); //206
   this.addMultiple(pipeParts);
 };
 
@@ -30,24 +30,33 @@ FartyTurd.Pipe.prototype.configure = function(x, y, speed, pipeData) {
   this.pipeData = pipeData;
 
   this.children.forEach(function (part, i, parts) {
-    if (i === 2) {
-      part.y = parts[1].bottom + this.pipeData.gapSize;
-    }
-    if (i === 3) {
+    switch (i) {
+      case 0:
+      part.height = this.pipeData.upperPipeHeight;
+      break;
+      case 1:
+      part.y = parts[0].bottom;
+      break;
+      case 2:
+      part.y = parts[1].bottom + this.pipeData.pipeGap;
+      break;
+      case 3:
       part.y = parts[2].bottom;
+      part.height = this.totalHeight - parts[2].bottom;
+      break;
     }
   }, this);
-  
+
   //make alive
-  this.alive = true; 
+  this.alive = true;
   this.isScored = false;
-  
+
   //set physics properties
   this.setAll('body.immovable', true);
   this.setAll('body.allowGravity', false);
   this.setAll('body.velocity.x', speed);
   this.setAll('x', x);
-  
+
 };
 
 FartyTurd.Pipe.prototype.kill = function(){
--- js/states/Game.js
+++ js/states/Game.js
@@ -3,38 +3,45 @@ var FartyTurd = FartyTurd || {};
 FartyTurd.GameState = {
 
   init: function() {
-    
+
     //pool of pipes
     this.pipePool = this.add.group();
-    
+
     //gravity
-    this.game.physics.arcade.gravity.y = 1000;    
-    
+    this.game.physics.arcade.gravity.y = 1000;
+
     //max jump distance
     this.maxJumpDistance = 120;
-    
+
     //move player with up key
     this.cursors = this.game.input.keyboard.createCursorKeys();
-    
-    //score 
+
+    //score
     this.currentScore = 0;
-    
+
     //level values
-    this.levelScoreBoundary = 3;
+    this.levelScoreBoundary = 10;
     this.levelSpeed = 100;
     this.minPipeSeparation = 50;
     this.maxPipeSeparation = 180;
-    this.minPipeGapSize = 80;
-    this.maxPipeGapSize = 130;
+    this.pipeConfig = {
+      maxHeight: 100,
+      minHeight: 20,
+      maxGap: 130,
+      minGap: 80
+    };
   },
   create: function() {
+    var fartSound,
+      style,
+      i;
     //moving background
     this.background = this.add.tileSprite(0, 0, this.game.world.width, this.game.world.height, 'background');
     this.background.tileScale.y = 2.75;
     this.background.tileScale.x = 2.75;
     this.background.autoScroll(-this.levelSpeed/6, 0);
     this.game.world.sendToBack(this.background);
-            
+
     //create the player
     this.player = this.add.sprite(100, 140, 'turd');
     this.player.anchor.setTo(0.5);
@@ -46,10 +53,10 @@ FartyTurd.GameState = {
     this.turdExplosion.minParticleSpeed.setTo(-200, -200);
     this.turdExplosion.maxParticleSpeed.setTo(200, 200);
     this.turdExplosion.gravity = 0;
-    
+
     //change player bounding box
     this.player.body.setSize(32, 24, 0, 0);
-    
+
     //hard-code first pipe
     this.currentPipe = new FartyTurd.Pipe(this.game, 250, 0, -this.levelSpeed, this.generateRandomPipe());
     this.pipePool.add(this.currentPipe);
@@ -66,39 +73,41 @@ FartyTurd.GameState = {
     this.fart.body.allowGravity = false;
     this.fart.anchor.setTo(0.5);
     this.fart.kill();
-    
+
     //fart sound
 
     this.fartSounds = [];
 
-    for (var i = 0; i < 4; i++) {
-      var fartSound = this.add.audio('fartSound' + i);
+    for (i = 0; i < 4; i++) {
+      fartSound = this.add.audio('fartSound' + i);
       fartSound.isPlaying = false;
-      fartSound.onPlay.add(function (s) {
-        s.isPlaying = true;
-        this.fart.reset(this.player.x + this.fart.customParams.offset.x, this.player.y + this.fart.customParams.offset.y);
-        this.fart.body.velocity.x = -this.levelSpeed;
-      }, this);
-      fartSound.onStop.add(function (s) {
-        s.isPlaying = false;
-        this.fart.kill();
-      }, this);
+      fartSound.onPlay.add(this.onSoundPlay, this);
+      fartSound.onStop.add(this.onSoundStop, this);
       this.fartSounds.push(fartSound);
-    };
-    
+    }
+
     this.createPipe();
-    
+
     //moving ground
     this.ground = this.add.tileSprite(0, this.game.world.height - 30, this.game.world.width, 30, 'ground');
     this.ground.autoScroll(-this.levelSpeed/2, 0);
-    
+
     //show number of coins
-    var style = {font: '30px Arial', fill: '#fff'};
+    style = {font: '30px Arial', fill: '#fff'};
     this.fartCountLabel = this.add.text(10, 20, '0', style);
-  },   
-  update: function() {    
+  },
+  onSoundPlay: function (sound) {
+    sound.isPlaying = true;
+    this.fart.reset(this.player.x + this.fart.customParams.offset.x, this.player.y + this.fart.customParams.offset.y);
+    this.fart.body.velocity.x = -this.levelSpeed;
+  },
+  onSoundStop: function (sound) {
+    sound.isPlaying = false;
+    this.fart.kill();
+  },
+  update: function() {
     if(this.player.alive) {
-      this.pipePool.forEachAlive(function(pipe, index){
+      this.pipePool.forEachAlive(function(pipe){
         this.game.physics.arcade.overlap(this.player, pipe, this.gameOver, null, this);
 
         if(!pipe.isScored && pipe.length && pipe.children[0].right < this.player.left) {
@@ -109,7 +118,7 @@ FartyTurd.GameState = {
         //check if a pipe needs to be killed
         if(pipe.length && pipe.children[0].right < 0) {
           pipe.kill();
-        }    
+        }
 
       }, this);
 
@@ -123,13 +132,13 @@ FartyTurd.GameState = {
       if(this.currentPipe.length && this.currentPipe.children[0].right < this.game.world.width) {
         this.createPipe();
       }
-      
+
       //check if the player needs to die
       if(this.player.bottom >= this.ground.top) {
         this.gameOver();
       }
     }
-     
+
   },
   isFartSoundPlaying: function () {
     var isPlaying = false;
@@ -158,10 +167,12 @@ FartyTurd.GameState = {
   },
   levelUp: function () {
     this.levelSpeed = this.levelSpeed++;
-    this.minPipeSeparation = Math.max(10, this.minPipeSeparation - 1);
-    this.maxPipeSeparation = Math.min(180, this.maxPipeSeparation - 1);
-    this.minPipeGapSize = Math.max(80, this.minPipeGapSize - 1);
-    this.maxPipeGapSize = Math.min(130, this.maxPipeGapSize - 1);
+    this.minPipeSeparation = Math.max(10, this.minPipeSeparation - 2);
+    this.maxPipeSeparation = Math.min(180, this.maxPipeSeparation - 2);
+    this.pipeConfig.maxHeight = Math.min(100, this.pipeConfig.maxHeight - 2);
+    this.pipeConfig.minHeight = Math.max(20, this.pipeConfig.minHeight - 2);
+    this.pipeConfig.maxGap = Math.min(130, this.pipeConfig.maxGap - 2);
+    this.pipeConfig.minGap = Math.max(80, this.pipeConfig.minGap - 2);
 
   },
   playerJump: function(){
@@ -169,13 +180,13 @@ FartyTurd.GameState = {
   },
   createPipe: function(){
     var nextPipeData = this.generateRandomPipe();
-      
+
     this.currentPipe = this.pipePool.getFirstDead();
-    
+
     if(!this.currentPipe) {
-      this.currentPipe = new FartyTurd.Pipe(this.game, this.game.world.width + nextPipeData.separation, 0, -this.levelSpeed, nextPipeData);   
+      this.currentPipe = new FartyTurd.Pipe(this.game, this.game.world.width + nextPipeData.separation, 0, -this.levelSpeed, nextPipeData);
     } else {
-      this.currentPipe.configure(this.game.world.width + nextPipeData.separation, 0, -this.levelSpeed, nextPipeData);   
+      this.currentPipe.configure(this.game.world.width + nextPipeData.separation, 0, -this.levelSpeed, nextPipeData);
     }
 
     this.pipePool.add(this.currentPipe);
@@ -183,53 +194,54 @@ FartyTurd.GameState = {
   generateRandomPipe: function() {
     return {
       separation: this.minPipeSeparation + Math.random() * (this.maxPipeSeparation - this.minPipeSeparation),
-      gapSize: this.minPipeGapSize + Math.random() * (this.maxPipeGapSize - this.minPipeGapSize)
+      upperPipeHeight: this.pipeConfig.minHeight + Math.random() * (this.pipeConfig.maxHeight - this.pipeConfig.minHeight),
+      pipeGap: this.pipeConfig.minGap + Math.random() * (this.pipeConfig.maxGap - this.pipeConfig.minGap)
     };
   },
-  gameOver: function(){
+  gameOver: function() {
     this.explodeTurd();
-    this.player.kill();    
+    this.player.kill();
     this.updateHighscore();
-    
+
     //game over overlay
     this.overlay = this.add.bitmapData(this.game.width, this.game.height);
     this.overlay.ctx.fillStyle = '#000';
     this.overlay.ctx.fillRect(0, 0, this.game.width, this.game.height);
-    
+
     //sprite for the overlay
     this.panel = this.add.sprite(0, this.game.height, this.overlay);
     this.panel.alpha = 0.55;
-    
+
     //overlay raising tween animation
     var gameOverPanel = this.add.tween(this.panel);
     gameOverPanel.to({y: 0}, 500);
-    
+
     //stop all movement after the overlay reaches the top
     gameOverPanel.onComplete.add(function(){
       this.ground.stopScroll();
       this.background.stopScroll();
-      
+
       var style = {font: '30px Arial', fill: '#fff'};
       this.add.text(this.game.width/2, this.game.height/2, 'GAME OVER', style).anchor.setTo(0.5);
 
       style = {font: '15px Arial', fill: '#fff'};
       this.add.text(this.game.width/2, this.game.height/2 + 22, 'Now wash your hands', style).anchor.setTo(0.5);
-      
+
       style = {font: '20px Arial', fill: '#fff'};
       this.add.text(this.game.width/2, this.game.height/2 + 50, 'High score: ' + this.highScore, style).anchor.setTo(0.5);
-      
+
       this.add.text(this.game.width/2, this.game.height/2 + 80, 'Your score: ' + this.currentScore, style).anchor.setTo(0.5);
-      
+
       style = {font: '10px Arial', fill: '#fff'};
       this.add.text(this.game.width/2, this.game.height/2 + 120, 'Tap to play again', style).anchor.setTo(0.5);
-      
+
       this.game.input.onDown.addOnce(this.restart, this);
-      
-      
+
+
     }, this);
-    
+
     gameOverPanel.start();
-    
+
   },
   explodeTurd: function () {
     this.turdExplosion.x = this.player.x;
@@ -241,22 +253,22 @@ FartyTurd.GameState = {
     //http://www.html5gamedevs.com/topic/13843-events-linger-after-remove-and-destroy/
     this.game.world.remove(this.background);
     this.game.world.remove(this.ground);
-    
+
     this.game.state.start('Game');
   },
   updateHighscore: function(){
     this.highScore = +localStorage.getItem('highScore');
-    
+
     //do we have a new high score
     if(this.highScore < this.currentScore){
       this.highScore = this.currentScore;
-      
+
       //save new high score
       localStorage.setItem('highScore', this.highScore);
     }
   }
 
-  
+
   /*render: function() {
     this.game.debug.body(this.player);
     this.game.debug.bodyInfo(this.player, 0, 30);
