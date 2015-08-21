1bb4fae22c89d4fe023a6f1cc5dfb949e2087f8a
Pappa <dr.pappa@gmail.com>
2015-08-21 16:13:44 +0100

    Loading bar

--- js/states/Boot.js
+++ js/states/Boot.js
@@ -4,21 +4,21 @@ var FartyTurd = FartyTurd || {};
 FartyTurd.BootState = {
   init: function() {
     //loading screen will have a white background
-    this.game.stage.backgroundColor = '#fff';  
-    
+    this.game.stage.backgroundColor = '#fff';
+
     //scaling options
     this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-    
+
     //have the game centered horizontally
     this.scale.pageAlignHorizontally = true;
     this.scale.pageAlignVertically = true;
 
     //physics system
-    this.game.physics.startSystem(Phaser.Physics.ARCADE);    
+    this.game.physics.startSystem(Phaser.Physics.ARCADE);
   },
   preload: function() {
     //assets we'll use in the loading screen
-    this.load.image('preloadbar', 'assets/images/preloader-bar.png');
+    this.load.image('loadingBar', 'assets/images/loadingBar.png');
   },
   create: function() {
     this.state.start('Preload');
--- js/states/Preload.js
+++ js/states/Preload.js
@@ -4,20 +4,20 @@ var FartyTurd = FartyTurd || {};
 FartyTurd.PreloadState = {
   preload: function() {
     //show loading screen
-    this.preloadBar = this.add.sprite(this.game.world.centerX, this.game.world.centerY, 'preloadbar');
+    this.preloadBar = this.add.sprite(this.game.world.centerX, this.game.world.centerY, 'loadingBar');
     this.preloadBar.anchor.setTo(0.5);
     this.preloadBar.scale.setTo(3);
 
     this.load.setPreloadSprite(this.preloadBar);
 
-    //load game assets 
+    //load game assets
     this.load.image('pipe', 'assets/images/pipe.png');
     this.load.image('pipe-up', 'assets/images/pipe-up.png');
     this.load.image('pipe-down', 'assets/images/pipe-down.png');
     this.load.image('ground', 'assets/images/ground.png');
     this.load.image('background', 'assets/images/background.png');
-    this.load.image('turd', 'assets/images/turd.png');   
-    this.load.image('turdParticle', 'assets/images/turdParticle.png'); 
+    this.load.image('turd', 'assets/images/turd.png');
+    this.load.image('turdParticle', 'assets/images/turdParticle.png');
     this.load.image('fart', 'assets/images/fart.png');
     this.load.audio('fartSound0', ['assets/audio/fart.mp3', 'assets/audio/fart.ogg']);
     this.load.audio('fartSound1', ['assets/audio/fart2.mp3', 'assets/audio/fart2.ogg']);
