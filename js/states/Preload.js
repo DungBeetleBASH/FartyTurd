63d9b88680ed28e5bd8327bdcd599bf1897d7e40
Robert Hill <dr.pappa@gmail.com>
2015-08-24 19:50:50 +0100

    Reorganising files

--- js/states/Preload.js
+++ js/states/Preload.js
@@ -11,18 +11,18 @@ FartyTurd.PreloadState = {
     this.load.setPreloadSprite(this.preloadBar);
 
     //load game assets
-    this.load.image('pipe', 'assets/images/pipe.png');
-    this.load.image('pipe-up', 'assets/images/pipe-up.png');
-    this.load.image('pipe-down', 'assets/images/pipe-down.png');
-    this.load.image('background', 'assets/images/background.png');
-    this.load.image('turd', 'assets/images/turd.png');
-    this.load.image('turdParticle', 'assets/images/turdParticle.png');
-    this.load.image('fart', 'assets/images/fart.png');
-    this.load.audio('splat', ['assets/audio/splat.mp3', 'assets/audio/splat.ogg']);
-    this.load.audio('fartSound0', ['assets/audio/fart.mp3', 'assets/audio/fart.ogg']);
-    this.load.audio('fartSound1', ['assets/audio/fart2.mp3', 'assets/audio/fart2.ogg']);
-    this.load.audio('fartSound2', ['assets/audio/fart3.mp3', 'assets/audio/fart3.ogg']);
-    this.load.audio('fartSound3', ['assets/audio/fart4.mp3', 'assets/audio/fart4.ogg']);
+    this.load.image('pipe', 'asset/images/pipe.png');
+    this.load.image('pipe-up', 'asset/images/pipe-up.png');
+    this.load.image('pipe-down', 'asset/images/pipe-down.png');
+    this.load.image('background', 'asset/images/background.png');
+    this.load.image('turd', 'asset/images/turd.png');
+    this.load.image('turdParticle', 'asset/images/turdParticle.png');
+    this.load.image('fart', 'asset/images/fart.png');
+    this.load.audio('splat', ['asset/audio/splat.mp3', 'asset/audio/splat.ogg']);
+    this.load.audio('fartSound0', ['asset/audio/fart.mp3', 'asset/audio/fart.ogg']);
+    this.load.audio('fartSound1', ['asset/audio/fart2.mp3', 'asset/audio/fart2.ogg']);
+    this.load.audio('fartSound2', ['asset/audio/fart3.mp3', 'asset/audio/fart3.ogg']);
+    this.load.audio('fartSound3', ['asset/audio/fart4.mp3', 'asset/audio/fart4.ogg']);
   },
   create: function() {
     this.state.start('Game');
