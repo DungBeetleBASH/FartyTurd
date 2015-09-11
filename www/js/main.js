160951d81a29692b14d4c88097be398e4514a383
Robert Hill <dr.pappa@gmail.com>
2015-09-11 19:25:18 +0100

    Fixing device orientation to portrait

--- www/js/main.js
+++ www/js/main.js
@@ -1,5 +1,10 @@
 var FartyTurd = FartyTurd || {};
 
+document.addEventListener("intel.xdk.device.ready", function() {
+    intel.xdk.device.setRotateOrientation("portrait");
+    intel.xdk.device.setAutoRotate(false);
+}, false);
+
 FartyTurd.game = new Phaser.Game(320, 480, Phaser.AUTO);
 
 FartyTurd.game.state.add('Boot', FartyTurd.BootState);
