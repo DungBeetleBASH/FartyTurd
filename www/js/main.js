a4471cf6d26aee84c0531c17b034e8e25d3370a5
Robert Hill <dr.pappa@gmail.com>
2015-09-15 18:26:33 +0100

    Pause/resume handling

--- www/js/main.js
+++ www/js/main.js
@@ -8,13 +8,21 @@ document.addEventListener("intel.xdk.device.ready", function(){
 
 document.addEventListener("intel.xdk.device.suspend", function() {
     console.log("intel.xdk.device.suspend");
+    FartyTurd.game.paused = true;
 }, false);
 
 document.addEventListener("intel.xdk.device.pause", function() {
     console.log("intel.xdk.device.pause");
+    FartyTurd.game.paused = true;
+}, false);
+
+document.addEventListener("intel.xdk.device.continue", function() {
+    FartyTurd.game.paused = false;
+    console.log("intel.xdk.device.continue");
 }, false);
 
 document.addEventListener("intel.xdk.device.resume", function() {
+    FartyTurd.game.paused = false;
     console.log("intel.xdk.device.resume");
 }, false);
 
