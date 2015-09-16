a8ea05facf9aa1a85e0ac172d18ab27a3e56be94
Robert Hill <dr.pappa@gmail.com>
2015-09-16 21:42:49 +0100

    Removing debug

--- www/js/main.js
+++ www/js/main.js
@@ -2,37 +2,38 @@ var FartyTurd = FartyTurd || {};
 
 document.addEventListener("intel.xdk.device.ready", function(){
     intel.xdk.device.setRotateOrientation("portrait");
-    intel.xdk.device.setAutoRotate(false);     
+    intel.xdk.device.setAutoRotate(false);
+    //console.log("intel.xdk.device.ready");
 }, false);
 
 document.addEventListener("intel.xdk.device.suspend", function() {
-    console.log("intel.xdk.device.suspend");
     FartyTurd.game.paused = true;
+    //console.log("intel.xdk.device.suspend");
 }, false);
 
 document.addEventListener("intel.xdk.device.pause", function() {
-    console.log("intel.xdk.device.pause");
     FartyTurd.game.paused = true;
+    //console.log("intel.xdk.device.pause");
 }, false);
 
 document.addEventListener("pause", function() {
-    console.log("pause");
     FartyTurd.game.paused = true;
+    //console.log("pause");
 }, false);
 
 document.addEventListener("intel.xdk.device.continue", function() {
     FartyTurd.game.paused = false;
-    console.log("intel.xdk.device.continue");
+    //console.log("intel.xdk.device.continue");
 }, false);
 
 document.addEventListener("intel.xdk.device.resume", function() {
     FartyTurd.game.paused = false;
-    console.log("intel.xdk.device.resume");
+    //console.log("intel.xdk.device.resume");
 }, false);
 
 document.addEventListener("resume", function() {
     FartyTurd.game.paused = false;
-    console.log("resume");
+    //console.log("resume");
 }, false);
 
 FartyTurd.game = new Phaser.Game(320, 480, Phaser.AUTO);
